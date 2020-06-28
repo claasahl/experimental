9e78c5c2056935c38d9132c3a5a1d8251bc85dab
Claas Ahlrichs <claasahl@gmail.com>
2020-06-28 11:35:52 +0200

    fetch data

--- tensorflow/index.ts
+++ tensorflow/index.ts
@@ -1,1 +1,47 @@
+import { render } from "@tensorflow/tfjs-vis"
+
+interface MatchboxCar {
+    mpg: number;
+    horsepower: number;
+}
+
+/**
+ * Get the car data reduced to just the variables we are interested
+ * and cleaned of missing data.
+ */
+async function getData(): Promise<MatchboxCar[]> {
+    const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json');
+    const carsData: { Miles_per_Gallon: number, Horsepower: number }[] = await carsDataReq.json();
+    const cleaned = carsData.map(car => ({
+        mpg: car.Miles_per_Gallon,
+        horsepower: car.Horsepower,
+    }))
+        .filter(car => (car.mpg != null && car.horsepower != null));
+
+    return cleaned;
+}
+
+async function run() {
+    // Load and plot the original input data that we are going to train on.
+    const data = await getData();
+    const values = data.map(d => ({
+        x: d.horsepower,
+        y: d.mpg,
+    }));
+
+    render.scatterplot(
+        { name: 'Horsepower v MPG' },
+        { values },
+        {
+            xLabel: 'Horsepower',
+            yLabel: 'MPG',
+            height: 300
+        }
+    );
+
+    // More code will be added below
+}
+
+document.addEventListener('DOMContentLoaded', run);
+
 console.log('Hello TensorFlow');
